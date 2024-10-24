c340407e0cc8cbcf4817ef78f05dcf81001b6d28
amoriotechnology <110466648+amoriotechnology@users.noreply.github.com>
2024-10-24 10:30:37 +0530

    Update medicare_tax.php

--- application/views/hr/reports/medicare_tax.php
+++ application/views/hr/reports/medicare_tax.php
@@ -151,7 +151,6 @@ $(".sidebar-mini").addClass('sidebar-collapse') ;
          { "data": "m_mtax" },
          { "data": "tm_mtax" },
          ],
-        "order": [4, 'desc'],
         "columnDefs": [{
             "orderable": false,
             "targets": [0, 7],
